13753439356837a62a1f7485b61ce1b82f595bc6
Phil <pizzapost.mail@gmail.com>
2025-04-27 21:12:22 +0200

    better spider effect climbing

--- src/main/java/de/pizzapost/minecraft_extra/effect/custom/SpiderEffect.java
+++ src/main/java/de/pizzapost/minecraft_extra/effect/custom/SpiderEffect.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.control.JumpControl;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
 // Climbing Effect by SameDifferent: https://github.com/samedifferent/TrickOrTreat/blob/master/LICENSE
@@ -17,12 +18,14 @@ public class SpiderEffect extends StatusEffect {
 
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (entity.horizontalCollision && entity.getPitch() >= 50) {
+        BlockPos blockPos1 = entity.getBlockPos().down();
+        BlockPos blockPos2 = entity.getBlockPos().down(2);
+        if (entity.horizontalCollision && entity.getPitch() >= 50 && !entity.getWorld().getBlockState(blockPos1).isSolidBlock(entity.getWorld(), blockPos1) && !entity.getWorld().getBlockState(blockPos2).isSolidBlock(entity.getWorld(), blockPos2)) {
             Vec3d initialVec = entity.getVelocity();
             Vec3d climbVec = new Vec3d(initialVec.x, -0.1D, initialVec.z);
             entity.setVelocity(climbVec.multiply(0.96D));
             return true;
-        } else if (entity.horizontalCollision && entity.getPitch() < 50) {
+        } else if (entity.horizontalCollision && entity.getPitch() < 50 && !entity.getWorld().getBlockState(blockPos1).isSolidBlock(entity.getWorld(), blockPos1)) {
             Vec3d initialVec = entity.getVelocity();
             Vec3d climbVec = new Vec3d(initialVec.x, 0.1D, initialVec.z);
             entity.setVelocity(climbVec.multiply(0.96D));
